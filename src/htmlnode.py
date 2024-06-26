00a91a96629506354ef3c5a4bb3e9907b8b99392
DavidIRL <david.white0911@gmail.com>
2024-06-26 15:35:01 -0400

    update htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -54,7 +54,7 @@ class ParentNode(HTMLNode):
         for child in self.children:
             children_html += child.to_html()
 
-        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
 
     def __repr__(self):
